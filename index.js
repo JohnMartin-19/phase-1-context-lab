baabe5592497881a6da93f5c814d1b97d0e8f053
John <johnnkonge2020@gmail.com>
2023-10-07 12:37:45 +0300

    not done

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 /* Your Code Here */
+const hourlyRate = 70; 
+function calculateWages(startDateStr, endDateStr, hourlyRate) {
+    // Parse the date strings to Date objects
+    const startDate = new Date(startDateStr);
+    const endDate = new Date(endDateStr);
+  
+    // Calculate the time difference in milliseconds
+    const timeDifference = endDate - startDate;
+  
+    // Calculate the number of days worked
+    const daysWorked = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    console.log(`Days Worked: ${daysWorked}`);
 
+    // Calculate the number of hours worked
+    const hoursWorked = (timeDifference / (1000 * 60 * 60)) % 24;
+    console.log(`Hours Worked: ${hoursWorked}`);
+
+    // Calculate the total wages
+    const totalWages = hoursWorked * hourlyRate;
+    console.log(`Total Wages: ${totalWages}`);
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
